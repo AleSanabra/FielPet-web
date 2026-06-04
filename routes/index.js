6af88d68a01f21d02c34adecec4eca4019f61d08
AleSanabra <62352381+AleSanabra@users.noreply.github.com>
2026-06-04 13:40:45 -0300

    actulizacion pre google

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res) => {
-    res.render('fielpet');
+
+  //Leo lista de nombre de fotos de mascotas
+  const carpetaMascotas = path.join(__dirname, '../','public/img/mascotas');
+
+  const imagenes = fs.readdirSync(carpetaMascotas)
+    .filter(archivo =>
+      /\.(jpg|jpeg|png|webp|gif)$/i.test(archivo)
+    )
+    .map(archivo => `/img/mascotas/${archivo}`);
+
+  // Muestro la pagina
+  res.render('fielpet', {
+    imagenes,
+    whatsapp: '5492804300220',
+    dominio: 'mascotaspiotrowsky.com.ar',
+  });
 });
 
 
@@ -10,14 +27,14 @@ router.get('/', (req, res) => {
 // Cualquier otra dirección, manda a index PERO permite archivos estáticos
 router.use((req, res, next) => {
 
-    // Si es un archivo estático, continuar
-    if (req.path.match(/\.(css|js|png|jpg|jpeg|gif|ico|svg|webp)$/)) {
-      return next();
-    }
-  
-    // Si no es archivo estático, redirigir al inicio
-    res.redirect('/');
-  });
+  // Si es un archivo estático, continuar
+  if (req.path.match(/\.(css|js|png|jpg|jpeg|gif|ico|svg|webp)$/)) {
+    return next();
+  }
+
+  // Si no es archivo estático, redirigir al inicio
+  res.redirect('/');
+});
 
 
 
